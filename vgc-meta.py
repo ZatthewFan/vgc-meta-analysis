a088740fd7c21d88393ddd3a456779b755f9ce76
Zatt <88600035+ZatthewFan@users.noreply.github.com>
2023-10-18 15:27:35 -0700

    added template for pokemon set

--- vgc-meta.py
+++ vgc-meta.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 
 BASE_URL = "https://victoryroadvgc.com/sv-rental-teams-2023/"
 
@@ -18,16 +17,41 @@ def get_pokepaste_links():
     pokepaste_links = driver.find_elements(By.XPATH, '//a[contains(@href, "pokepast.es")]')
     for link in pokepaste_links:
         pokepastes.append(link.get_attribute('href'))
-    
-def open_all_pokepastes():
+
+def parse_pokepaste():
+    name = None
+    item = None
+    ability = None
+    tera = None
+    ev = {
+        "HP" : 0, 
+        "Atk": 0, 
+        "Def": 0, 
+        "SpA": 0, 
+        "SpD": 0, 
+        "Spe": 0
+    }
+    nature = ""
+    iv = {
+        "HP" : 31, 
+        "Atk": 31, 
+        "Def": 31, 
+        "SpA": 31, 
+        "SpD": 31, 
+        "Spe": 31
+    }
+    moveset = [None, None, None, None]
+
+def parse_all_pokepastes():
     for paste in pokepastes:
         driver.get(paste)
+        parse_pokepaste()
 
 
 def main():
     open_webpage()
     get_pokepaste_links()
-    open_all_pokepastes()
+    parse_all_pokepastes()
     driver.quit()
 
 main()
