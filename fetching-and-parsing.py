4ca99ff952de294630ddd23de4b08c98ff0b1106
Zatt <88600035+ZatthewFan@users.noreply.github.com>
2023-10-21 07:50:20 -0700

    changed parse_mon() to be more dynamic

--- fetching-and-parsing.py
+++ fetching-and-parsing.py
@@ -13,16 +13,19 @@ options = Options()
 options.add_argument("load-extension=./extensions/ublock-origin")
 driver = webdriver.Chrome(options=options)
 
-pokepastes = []
-
 def open_webpage():
     driver.get(BASE_URL)
 
+# returns a list of all links containing pokepast.es
 def get_pokepaste_links():
+    pokepastes = []
     pokepaste_links = driver.find_elements(By.XPATH, '//a[contains(@href, "pokepast.es")]')
     for link in pokepaste_links:
         pokepastes.append(link.get_attribute('href'))
+    
+    return pokepastes
 
+# helper function to process the ev and iv 
 def parse_ev_iv(which, pkm, stats):
     stats_dict = {}
 
@@ -42,19 +45,18 @@ def parse_ev_iv(which, pkm, stats):
             value = item[1]
             pkm.set_iv(**{key: value})
 
-def parse_moveset(pkm, *args):
-    if len(args) > 4:
-        return
-    
+def parse_moveset(pkm, moves):
     moveset = []
     
-    for move in args:
+    # getting rid of extra spaces and hyphens
+    for move in moves:
         move = move.split("-")[1]
         move = move.strip()
         moveset.append(move)
     
     pkm.set_moveset(moveset)
 
+# parses the data for each individual pokemon
 def parse_mon(pkm_set):
     pkm = Pokemon()
     pkm_set = pkm_set.text.splitlines()
@@ -62,24 +64,35 @@ def parse_mon(pkm_set):
     if "EV" not in pkm_set[4]:
         return
     
+    # name, item, and ability are sure to be in the pokepaste
     pkm.set_name(pkm_set[0].split("@")[0].strip())
     pkm.set_item(pkm_set[0].split("@")[1].strip())
     pkm.set_ability(pkm_set[1].split(":")[1].strip())
-    pkm.set_tera(pkm_set[3].split(":")[1].strip())
-    parse_ev_iv("ev", pkm, re.split(": |/", pkm_set[4].strip())[1:])
-    pkm.set_nature(pkm_set[5].split()[0].strip())
-    if "IV" in pkm_set[6]:
-        parse_ev_iv("iv", pkm, re.split(": |/", pkm_set[6].strip())[1:])
-        parse_moveset(pkm, pkm_set[7], pkm_set[8], pkm_set[9], pkm_set[10])
-    else:
-        parse_moveset(pkm, pkm_set[6], pkm_set[7], pkm_set[8], pkm_set[9])
+    
+    # Tera Type, EVs, Nature, and IVs might not be in the pokepaste or might not be in a fixed line
+    # this loops through every line, checking whether it's any of the attributes we're looking for, and updates the pokemon set
+    for line in pkm_set:
+        if "Tera Type" in line:
+            pkm.set_tera(line.split(":")[1].strip())
+        elif "EVs" in line:
+            parse_ev_iv("ev", pkm, re.split(": |/", line.strip())[1:])
+        elif "Nature" in line:
+            pkm.set_nature(line.split()[0].strip())
+        elif "IVs" in line:
+            parse_ev_iv("iv", pkm, re.split(": |/", line.strip())[1:])
+    
+    # last 4 lines are definitely the moves
+    parse_moveset(pkm, pkm_set[-4:])
+    
     return pkm
 
+# stratifies the pokepaste for each pokemon, and iterates through them
 def parse_pokepaste():
     sets = driver.find_elements(By.CSS_SELECTOR, 'pre')
     vgc_format = driver.find_element(By.CSS_SELECTOR, 'aside p').text[8:]
     team = Team(vgc_format)
     
+    # format checker
     if (vgc_format != CURRENT_FORMAT):
         return
     
@@ -94,7 +107,7 @@ def parse_pokepaste():
     return team
 
 
-def parse_all_pokepastes():
+def parse_all_pokepastes(pokepastes):
     teams = []
     for pokepaste in pokepastes:
         driver.get(pokepaste)
@@ -121,21 +134,33 @@ def testing():
     
     pkm = Pokemon()
     parse_ev_iv("ev", pkm, re.split(": |/", set[4])[1:])
-    parse_moveset(pkm, set[7], set[8], set[9], set[10])
+    parse_moveset(pkm, set[-4:])
     print(pkm.get_moveset())
+    
+    # for line in set:
+    #     if "Tera Type" in line:
+    #         pkm.set_tera(line.split(":")[1].strip())
+    #         print(pkm.get_tera())
+    #     elif "EVs" in line:
+    #         parse_ev_iv("ev", pkm, re.split(": |/", line.strip())[1:])
+    #         print(pkm.get_ev())
+    #     elif "Nature" in line:
+    #         pkm.set_nature(line.split()[0].strip())
+    #         print(pkm.get_nature())
+    #     elif "IVs" in line:
+    #         parse_ev_iv("iv", pkm, re.split(": |/", line.strip())[1:])
+    #         print(pkm.get_iv())
 
 def teams_to_csv(teams):
     # TODO complete
     return
 
-def main():
+if __name__ == "__main__":
     open_webpage()
-    get_pokepaste_links()
-    teams = parse_all_pokepastes()
+    pokepastes = get_pokepaste_links()
+    teams = parse_all_pokepastes(pokepastes)
     
     teams_to_csv(teams)
     driver.quit()
     
     # testing()
-
-main()
