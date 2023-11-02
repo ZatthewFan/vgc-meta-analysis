5e4bf6132930ec1e8679a2d99c91cf99777ccef1
Zatt <88600035+ZatthewFan@users.noreply.github.com>
2023-11-02 01:41:22 -0700

    added type hints for both Pokemon.py and Team.py

--- Pokemon.py
+++ Pokemon.py
@@ -25,55 +25,55 @@ class Pokemon:
     
     
     
-    def set_name(self, name):
+    def set_name(self, name: str) -> None:
         self.name = name
     
-    def set_item(self, item):
+    def set_item(self, item: str) -> None:
         self.item = item
     
-    def set_ability(self, ability):
+    def set_ability(self, ability: str) -> None:
         self.ability = ability
     
-    def set_tera(self, tera):
+    def set_tera(self, tera: str) -> None:
         self.tera = tera
     
-    def set_ev(self, **kwargs):
+    def set_ev(self, **kwargs: list[str | int]) -> None:
         for key, value in kwargs.items():
             self.ev[key] = value
     
-    def set_nature(self, nature):
+    def set_nature(self, nature: str) -> None:
         self.nature = nature
     
-    def set_iv(self, **kwargs):
+    def set_iv(self, **kwargs: list[str | int]) -> None:
         for key, value in kwargs.items():
             self.iv[key] = value
     
-    def set_moveset(self, moveset):
+    def set_moveset(self, moveset: list[str]) -> None:
         self.moveset[0] = moveset[0]
         self.moveset[1] = moveset[1]
         self.moveset[2] = moveset[2]
         self.moveset[3] = moveset[3]
     
-    def get_name(self):
+    def get_name(self) -> str:
         return self.name
     
-    def get_item(self):
+    def get_item(self) -> str:
         return self.item
     
-    def get_ability(self):
+    def get_ability(self) -> str:
         return self.ability
     
-    def get_tera(self):
+    def get_tera(self) -> str:
         return self.tera
     
-    def get_ev(self):
+    def get_ev(self) -> dict[str, int]:
         return self.ev
     
-    def get_nature(self):
+    def get_nature(self) -> str:
         return self.nature
     
-    def get_iv(self):
+    def get_iv(self) -> dict[str, int]:
         return self.iv
     
-    def get_moveset(self):
+    def get_moveset(self) -> list[str]:
         return self.moveset
--- Team.py
+++ Team.py
@@ -1,29 +1,30 @@
+from Pokemon import Pokemon
 class Team:
     format = None
     teamlist = []
     is_ots = False
     
-    def __init__(self, format):
+    def __init__(self, format: str) -> None:
         self.format = format
         self.teamlist = []
     
-    def add_pkm(self, pkm):
+    def add_pkm(self, pkm: Pokemon) -> None:
         self.teamlist.append(pkm)
         
-    def set_format(self, format):
+    def set_format(self, format: str) -> None:
         self.format = format
     
-    def set_teamlist(self, teamlist):
+    def set_teamlist(self, teamlist: list[Pokemon]) -> None:
         self.teamlist = teamlist
         
-    def set_ots(self, bool):
+    def set_ots(self, bool: bool) -> None:
         self.is_ots = bool
     
-    def get_format(self):
+    def get_format(self) -> str:
         return self.format
     
-    def get_teamlist(self):
+    def get_teamlist(self) -> list[Pokemon]:
         return self.teamlist
     
-    def get_ots(self):
+    def get_ots(self) -> bool:
         return self.is_ots
