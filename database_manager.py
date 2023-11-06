e1a1a8d4e0e505509176682b070a1d852b91b58e
Zatt <88600035+ZatthewFan@users.noreply.github.com>
2023-11-06 13:44:01 -0800

    changed database_manager.py from a class object to a script

--- database_manager.py
+++ database_manager.py
@@ -3,18 +3,18 @@ import os
 from dotenv import find_dotenv, load_dotenv
 import sys
 
-class DatabaseManager:
-    dotenv_path = find_dotenv()
-    load_dotenv(dotenv_path)
-    
-    MONGO_USER = os.getenv("MONGO_USER")
-    MONGO_PASS = os.getenv("MONGO_PASS")
-    MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
-    
-    try:
-        client = pymongo.MongoClient(f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@{MONGO_CLUSTER}.b3optnw.mongodb.net/?retryWrites=true&w=majority")
-    except pymongo.errors.ConfigurationError:
-        print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
-        sys.exit(1)
-    
-    db = client.myDatabase
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASS = os.getenv("MONGO_PASS")
+MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
+CONNECTION_STRING = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@{MONGO_CLUSTER}.b3optnw.mongodb.net/?retryWrites=true&w=majority"
+
+def get_database():
+    client = pymongo.MongoClient(CONNECTION_STRING)
+    return client["vgc_teams"]
+
+if __name__ == "__main__":
+    db = get_database()
